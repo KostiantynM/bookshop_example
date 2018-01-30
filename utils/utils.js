89d22c198ea6abcd7bf8f90be53e2debb66b9c9f
Kostiantyn Manko <kostiantyn.manko@gmail.com>
2018-01-30 21:20:23 +0200

    public settings

--- utils/utils.js
+++ utils/utils.js
@@ -2,17 +2,15 @@ var mongoose = require('mongoose');
 var config = require('../etc/config.json');
 
 module.exports = function setUpConnection(env) {
-	if('undefined' == typeof env){
-		env='development';
-	}
-	mongoose.connect(
-		`mongodb://kostiantyn.manko:yarrow13@ds263367.mlab.com:63367/bookshop`
-		/*`mongodb://${config[env].db.host}:${config[env].db.port}/${config[env].db.name}`*/, 
-		{ 
-			useMongoClient: true,
-			socketTimeoutMS: 0,
-			keepAlive: true,
-			reconnectTries: 30 
-		}
-	);
+  if ('undefined' == typeof env) {
+    env = 'development';
+  }
+  mongoose.connect(
+  // `mongodb://kostiantyn.manko:password@ds263367.mlab.com:63367/bookshop`
+  `mongodb://${config[env].db.host}:${config[env].db.port}/${config[env].db.name}`, {
+    useMongoClient: true,
+    socketTimeoutMS: 0,
+    keepAlive: true,
+    reconnectTries: 30
+  });
 }
